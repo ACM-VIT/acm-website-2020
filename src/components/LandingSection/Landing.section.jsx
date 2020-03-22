3d200fb86540cc2784fcb0f21411d73a7155fa0d
HelixW <shreyas.2000@hotmail.com>
2020-03-22 23:14:28 +0530

    New folder structure

---
+++ src/components/LandingSection/Landing.section.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Landing = props => {
+  return <section>ABC</section>;
+};
+
+Landing.section.propTypes = {};
+
+export default Landing;
