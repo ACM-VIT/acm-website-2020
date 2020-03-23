033320910694c263d6272c2847af3e6077661fcf
HelixW <shreyas.2000@hotmail.com>
2020-03-23 09:34:12 +0530

    Navbar done

--- src/components/layout/NavLink.jsx
+++ src/components/layout/NavLink.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const NavLink = props => {
-  const { children } = props;
+  const { children, active } = props;
 
   return (
-    <div className="mx-4 cursor-pointer hover:text-blue-300">{children}</div>
+    <div
+      className={`mx-4 cursor-pointer hover:text-blue-300 ${active &&
+        'text-acm-blue'}`}
+    >
+      {children}
+    </div>
   );
 };
 
 NavLink.propTypes = {
-  children: PropTypes.string.isRequired
+  children: PropTypes.string.isRequired,
+  active: PropTypes.bool.isRequired
 };
 
 export default NavLink;
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,25 +1,30 @@
 /* eslint-disable react/no-array-index-key */
 import React, { useState } from 'react';
 
+import { ReactComponent as AcmLogo } from '../../vectors/AcmLogo.svg';
 import NavLink from './NavLink';
 
 const Navbar = () => {
   const [links] = useState([
-    { id: 1, text: 'Home' },
-    { id: 2, text: 'About' },
-    { id: 3, text: 'Team' },
-    { id: 4, text: 'Blog' },
-    { id: 5, text: 'Projects' },
-    { id: 6, text: 'Contact Us' },
-    { id: 7, text: 'Gallery' }
+    { id: 1, text: 'Home', active: true },
+    { id: 2, text: 'About', active: false },
+    { id: 3, text: 'Team', active: false },
+    { id: 4, text: 'Blog', active: false },
+    { id: 5, text: 'Projects', active: false },
+    { id: 6, text: 'Contact Us', active: false },
+    { id: 7, text: 'Gallery', active: false }
   ]);
 
   return (
     <header className="bg-black text-white h-20 flex items-center px-16 justify-between">
-      <div>ACM LOGO</div>
+      <div>
+        <AcmLogo />
+      </div>
       <div className="flex">
         {links.map(link => (
-          <NavLink key={link.id}>{link.text}</NavLink>
+          <NavLink active={link.active} key={link.id}>
+            {link.text}
+          </NavLink>
         ))}
       </div>
     </header>
