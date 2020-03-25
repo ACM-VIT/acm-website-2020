041f89006acdf7e275afa5fb8626b7e17c213d41
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-03-25 19:05:42 +0530

    Added hamburger menu

--- src/components/HamburgerNavSection/HamburgerNav.jsx
+++ src/components/HamburgerNavSection/HamburgerNav.jsx
@@ -1,33 +1,10 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
-import HamburgerNav from './HamburgerNav.component';
 
-
-const HamburgerNavbar = () => {
-  const [links] = useState([
-    { id: 1, text: 'Home', scrollTo: 'landing-section' },
-    { id: 2, text: 'About', scrollTo: 'about-section' },
-    { id: 3, text: 'Team', scrollTo: 'team-section' },
-    { id: 4, text: 'Events', scrollTo: 'events-section' },
-    { id: 5, text: 'Blogs', scrollTo: 'blogs-section' },
-    { id: 6, text: 'Projects', scrollTo: 'projects-section' },
-    { id: 7, text: 'Contact Us', scrollTo: 'contact-section' },
-    { id: 8, text: 'Gallery', scrollTo: 'contact-section' }
-  ]);
-
+const HamburgerNav = () => {
   return(
-    <Menu right>
-      <Menu>
-        {links.map(links => (
-          <HamburgerNav
-            key={links.id} 
-            text={links.text}
-            scrollTo={links.scrollTo}
-          />
-        ))}
-      </Menu>
-    </Menu>
+    <a href
   );
 };
 
-export default HamburgerNavbar;
+export default HamburgerNav;
