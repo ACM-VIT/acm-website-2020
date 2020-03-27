4247166182f95159823c70c4d24553d8f2ad6b0b
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-03-27 16:17:54 +0530

    Changes to the hamnurger menu

--- src/components/HamburgerNavSection/HamburgerNav.jsx
+++ src/components/HamburgerNavSection/HamburgerNav.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-duplicates */
+/* eslint-disable no-shadow */
 /* eslint-disable react/jsx-indent */
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
@@ -9,16 +11,18 @@ const HamburgerNav = () => {
   const [links] = useState(LINKS);
   
   return(
-    <Menu className="text-center text-white text-xl top-0">
-        {links.map(links => (
-          <a
-            key={links.id}
-            href={`${window.location.origin}/#${links.scrollTo}`}
-          >
-            {links.text}
-          </a>
-        ))}
-    </Menu>
+    <div className="md:hidden">
+      <Menu className="text-center text-white text-xl top-0">
+          {links.map(links => (
+            <a
+              key={links.id}
+              href={`${window.location.origin}/#${links.scrollTo}`}
+            >
+              {links.text}
+            </a>
+          ))}
+      </Menu>
+    </div>
   );
 };
 
