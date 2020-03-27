729cce3f6baf313f0628bd8546289a2f8852b067
Ayithapu Sai Rithvik <rithvikayithapu@gmail.com>
2020-03-27 14:51:22 +0530

    Resolved issue with event page

--- src/components/HamburgerNavSection/HamburgerNav.jsx
+++ src/components/HamburgerNavSection/HamburgerNav.jsx
@@ -1,18 +1,23 @@
 /* eslint-disable react/jsx-indent */
 import React from 'react';
 import { slide as Menu } from 'react-burger-menu';
+import { useState } from 'react';
+import { LINKS } from '../../DataStore';
 
 const HamburgerNav = () => {
+
+  const [links] = useState(LINKS);
   
   return(
     <Menu className="text-center text-white text-xl top-0">
-        <a id="home" className="menu-item" href="#landing-section">Home</a>
-        <a id="about" className="menu-item" href="#about-section">About Us</a>
-        <a id="team" className="menu-item" href="#team-section">Team</a>
-        <a id="events" className="menu-item" href="#events-section">Events</a>
-        <a id="blogs" className="menu-item" href="#blogs-section">Blogs</a>
-        <a id="projects" className="menu-item" href="#projects-section">Projects</a>
-        <a id="contact-us" className="menu-item" href="#contact-section">Contact Us</a>
+        {links.map(links => (
+          <a
+            key={links.id}
+            href={`${window.location.origin}/#${links.scrollTo}`}
+          >
+            {links.text}
+          </a>
+        ))}
     </Menu>
   );
 };
