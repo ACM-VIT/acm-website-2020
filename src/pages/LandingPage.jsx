8b76338e708a0c9e7265c4e2bf883fdde6d884d5
HelixW <shreyas.2000@hotmail.com>
2020-03-29 11:21:06 +0530

    Chatbot integrated

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { transitions, positions, Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic';
@@ -13,6 +13,7 @@ import About from '../components/AboutSection/About.section';
 import Team from '../components/TeamSection/Team.section';
 import Blog from '../components/BlogSection/Blog.section';
 import ChatBot from '../components/ChatBotSection/ChatBot';
+import { ReactComponent as FAQ } from '../vectors/Chatbot.svg';
 
 // Alert options
 const options = {
@@ -23,16 +24,10 @@ const options = {
 };
 
 const LandingPage = ({ findOffset }) => {
+  const [showBot, setShowBot] = useState(false);
+
   return (
     <>
-      <iframe
-        allow="microphone;"
-        title="ChatBot"
-        width="350"
-        height="430"
-        className="bg-blue bottom-0 right-0 mr-0 mb-0 fixed"
-        src="https://console.dialogflow.com/api-client/demo/embedded/acm-faq"
-      />
       <AlertProvider template={AlertTemplate} {...options}>
         <Landing findOffset={offsetY => findOffset(offsetY, 0)} />
         <About findOffset={offsetY => findOffset(offsetY, 1)} />
@@ -41,6 +36,29 @@ const LandingPage = ({ findOffset }) => {
         <Blog findOffset={offsetY => findOffset(offsetY, 4)} />
         <Project findOffset={offsetY => findOffset(offsetY, 5)} />
         <Contact findOffset={offsetY => findOffset(offsetY, 6)} />
+        <button
+          className={`fixed rounded-full mr-8 flex justify-center items-center right-0 z-50 focus:outline-none ${
+            !showBot
+              ? `bottom-0 w-16 h-16 bg-acm-blue mb-8`
+              : `top-0 bg-red-600 w-8 h-8 mt-8`
+          }`}
+          type="button"
+          onClick={() => setShowBot(!showBot)}
+        >
+          {!showBot ? (
+            <FAQ />
+          ) : (
+            <img
+              src={`${process.env.PUBLIC_URL}/assets/images/CloseWhite.png`}
+              alt="Close chatbot"
+              height="20rem"
+              width="20rem"
+            />
+          )}
+        </button>
+        <div className={!showBot ? 'hidden' : 'fixed bottom-0 right-0 z-50'}>
+          <ChatBot />
+        </div>
       </AlertProvider>
     </>
   );
